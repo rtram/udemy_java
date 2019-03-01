393089d9170f8ced4020ea6f0ff5f62382d54c50
Robin Tram <robin.tram@gmail.com>
2019-03-01 12:52:16 -0500

    Integer Data Type

--- helloWorld/src/Hello.java
+++ helloWorld/src/Hello.java
@@ -4,5 +4,11 @@ public class Hello {
 
         System.out.println("Hello, World!");
 
+        int myFirstNumber = 10 + 5;
+
+        int myThirdNumber = myFirstNumber * 2;
+
+
+        System.out.println(1000 - myThirdNumber);
     }
 }
