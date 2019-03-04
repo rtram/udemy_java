1aeece230d07230863d1a5957edf16fdf6dd3d41
Robin Tram <robin.tram@gmail.com>
2019-03-04 12:59:35 -0500

    Complete CharAndBoolean

--- CharAndBoolean/src/com/robintram/Main.java
+++ CharAndBoolean/src/com/robintram/Main.java
@@ -3,6 +3,21 @@ package com.robintram;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was:" + myChar);
+
+        boolean isMale = true;
+        System.out.println("Am I Male? " + isMale);
+
+//        1. Find the code for the registered symbol on the same line as the copyright symbol.
+//        2. Create a variable of type char and assign it the unicode value for that symbol.
+//        3. Display in on screen.
+
+//        1 && 2.
+        char registered = '\u00AE';
+
+//        3.
+        System.out.println("Registered Symbol " + registered);
     }
 }
