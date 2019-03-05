2f993d3cd5b03249a7cfde3476975c2d527b315d
Robin Tram <robin.tram@gmail.com>
2019-03-05 11:47:48 -0500

    Complete String

--- Strings/src/com/robintram/Main.java
+++ Strings/src/com/robintram/Main.java
@@ -4,6 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-  
+        String myString = "This is a string";
+        System.out.println("myStrng is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myStrng is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println(lastString);
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString value " + lastString);
     }
 }
