97778f9992b680dd3fb99184ca437472767f2309
Robin Tram <robin.tram@gmail.com>
2019-03-12 12:36:33 -0400

    Complete Operators

--- Operators/src/com/robintram/Main.java
+++ Operators/src/com/robintram/Main.java
@@ -45,8 +45,48 @@ public class Main {
         if (topScore == 100)
             System.out.println("You got the high score!");
 
-        int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100)
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100))
             System.out.println("Greater than top score and less than 100");
+
+        if((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of these tests is true");
+
+        int newValue = 50;
+        if (newValue == 50)
+            System.out.println("This is true");
+
+        boolean isCar = true;
+        if(isCar)
+            System.out.println("This is not supposed to happen.");
+
+        boolean wasCar = isCar ? true : false;
+
+        if(wasCar)
+            System.out.println("wasCar is true");
+
+//        1. Create a double variable with the value 20
+//        2. Create a second variable of type double with the value 80
+//        3. Add both numbers up and multiply by 25
+//        4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+//        5. Write an "if" statement that displays a message "Total was over the limit"
+//            if the remaining total (#4) is equal to 20 or less.
+
+//        1.
+        double number = 20;
+
+//        2.
+        double number1 = 80;
+
+//        3.
+        double total = (number + number1) * 25;
+
+//        4.
+        double remainder = total % 40;
+        System.out.println(remainder);
+
+//        5.
+        if(remainder <= 20)
+            System.out.println("Total was over the limit");
     }
 }
