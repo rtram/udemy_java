7dbc194f1f225f7841d08c785165b9393536e2c5
Robin Tram <robin.tram@gmail.com>
2019-03-12 14:16:19 -0400

    Complete Keyword and Expressions

--- KeywordsAndExpressions/src/com/robintram/Main.java
+++ KeywordsAndExpressions/src/com/robintram/Main.java
@@ -3,6 +3,25 @@ package com.robintram;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+//    a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+
+//        In the following code that I will type below, write down what parts of the code are expressions
+
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+//        Expressions: score = 100, score = 0, score > 99, "You go the the high score!"
     }
 }
