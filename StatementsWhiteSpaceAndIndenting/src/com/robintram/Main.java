6879f8394ff11d33c1cb87bd435e32c8949d8683
Robin Tram <robin.tram@gmail.com>
2019-03-12 14:23:13 -0400

    Complete Statements White Space and Indenting

--- StatementsWhiteSpaceAndIndenting/src/com/robintram/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/com/robintram/Main.java
@@ -3,6 +3,20 @@ package com.robintram;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myVariable = 50;
+
+        myVariable++;
+
+        myVariable--;
+
+        System.out.println("This is a statement");
+
+        System.out.println("This is " +
+                "another " +
+                "still more.");
+
+//        Below are two statements.
+        int anotherVariable = 50; myVariable--;
     }
 }
