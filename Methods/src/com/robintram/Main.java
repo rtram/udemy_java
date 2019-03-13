4d6581921a20b1007c3a11505cf16f16966a2211
Robin Tram <robin.tram@gmail.com>
2019-03-13 13:23:31 -0400

    Complete Methods Part 1

--- Methods/src/com/robintram/Main.java
+++ Methods/src/com/robintram/Main.java
@@ -3,22 +3,19 @@ package com.robintram;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 900;
-        int levelCompleted = 5;
-        int bonus = 100;
+        calculateScore(true, 800, 5, 100);
 
+        calculateScore(true, 10000, 8, 200);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        } else {
+            return -1;
         }
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }    }
+    }
 }
